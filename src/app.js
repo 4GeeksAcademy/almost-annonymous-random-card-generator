74fcd4f3982d5df9f13a0d875153ea77e175db0f
almost-annonymous <anam.dhala@gmail.com>
2025-04-23 22:31:30 +0000

    changed code to make into blackjack game

--- src/app.js
+++ src/app.js
@@ -1,56 +1,85 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-//list different suits
-const SUITS = [{
-  type: "spades",
-  link: "https://static-00.iconduck.com/assets.00/spades-icon-1793x2048-pazqul4w.png"
-}, {
-  type: "diamonds",
-  link: "https://static-00.iconduck.com/assets.00/diamond-suit-emoji-750x1024-37v25znp.png" 
-}, {
-  type: "hearts",
-  link: "https://static-00.iconduck.com/assets.00/heart-suit-emoji-512x457-yvmca1xi.png" 
-}, {
-  type: "clubs",
-  link: "https://static-00.iconduck.com/assets.00/black-club-suit-emoji-256x256-gpq4i01e.png" 
-}];
-const NUMBERS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+// List different suits
+const SUITS = [
+  {
+    type: "spades",
+    link: "https://static-00.iconduck.com/assets.00/spades-icon-1793x2048-pazqul4w.png"
+  },
+  {
+    type: "diamonds",
+    link: "https://static-00.iconduck.com/assets.00/diamond-suit-emoji-750x1024-37v25znp.png"
+  },
+  {
+    type: "hearts",
+    link: "https://static-00.iconduck.com/assets.00/heart-suit-emoji-512x457-yvmca1xi.png"
+  },
+  {
+    type: "clubs",
+    link: "https://static-00.iconduck.com/assets.00/black-club-suit-emoji-256x256-gpq4i01e.png"
+  }
+];
 
+const NUMBERS = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+
+// Get a random item from a list
 function getRandom(list) {
   const randomIndex = Math.floor(Math.random() * list.length);
   return list[randomIndex];
-};
+}
 
-function refreshCard(event) {
-  // pick a suit
+// Generate a random card
+function getRandomCard() {
   const suit = getRandom(SUITS);
-  // pick an image
   const number = getRandom(NUMBERS);
-  //get the image for the suit
-  const suitImage = suit.link;
-  // get ahold of the img tag, and update the source attribute
-  const symbolOne = document.querySelector("#symbolOne");
-  symbolOne.src = suitImage;
-  const symbolTwo = document.querySelector("#symbolTwo");
-  symbolTwo.src = suitImage;
-  // get ahold of the card body, and update textContent with bio
-  const cardBody = document.querySelector("h1");
-  cardBody.textContent = number;
-};
+  return { suit, number };
+}
 
+// Update visuals for a card
+function updateCardVisual(card, topSelector, bottomSelector, bodySelector) {
+  document.querySelector(topSelector).src = card.suit.link;
+  document.querySelector(bottomSelector).src = card.suit.link;
+  document.querySelector(bodySelector).textContent = card.number;
+}
 
-window.onload = function () {
-  refreshCard();
-  // get ahold of the button
-  const button = document.querySelector("#refresh-btn");
-  // prepare this button to refresh a friend
-  button.addEventListener(
-      "click",
-      refreshCard
+// Card One: just generate and show
+function refreshCardOne() {
+  const card = getRandomCard();
+  updateCardVisual(card, "#cardOneTop", "#cardOneBottom", "#cardOneBody");
+  return card;
+}
+
+// Card Two: keep picking until it’s not the same as Card One
+function refreshCardTwo(forbiddenCard) {
+  let card;
+  do {
+    card = getRandomCard();
+  } while (
+    card.suit.type === forbiddenCard.suit.type &&
+    card.number === forbiddenCard.number
   );
+  updateCardVisual(card, "#cardTwoTop", "#cardTwoBottom", "#cardTwoBody");
+}
+
+// On page load
+window.onload = function () {
+  const forbiddenCard = refreshCardOne();
+  refreshCardTwo(forbiddenCard);
 };
+
+// Yes button (you won)
+const yesButton = document.querySelector("#yes-btn");
+yesButton.addEventListener("click", function () {
+  alert("Yay! You Won! Refresh to play again");
+});
+
+// Play Again button
+const playAgainButton = document.querySelector("#playagain-btn");
+playAgainButton.addEventListener("click", function () {
+  const forbiddenCard = refreshCardOne();
+  refreshCardTwo(forbiddenCard);
+});
